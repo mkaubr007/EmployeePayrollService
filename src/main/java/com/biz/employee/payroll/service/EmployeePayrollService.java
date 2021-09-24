039d290366935db2d4c9a3b37a776d6906a716e0
mkaubr007 <mkaubr007@gmail.com>
2021-09-24 16:19:21 +0530

    ReaAndWriteWithConsole added

--- src/main/java/com/biz/employee/payroll/service/EmployeePayrollService.java
+++ src/main/java/com/biz/employee/payroll/service/EmployeePayrollService.java
@@ -2,31 +2,28 @@ package com.biz.employee.payroll.service;
 import com.biz.employee.payroll.model.EmployeePayRoll;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    static  Scanner scan=new Scanner(System.in);
-    static List<EmployeePayRoll> list=new ArrayList<>();
-    public static void main(String[] args) {
+    ArrayList<EmployeePayRoll> employeePayRolls;
+    Scanner scan = new Scanner(System.in);
+
+    public EmployeePayrollService(ArrayList<EmployeePayRoll> employeePayRolls) {
+        this.employeePayRolls = employeePayRolls;
+    }
+
+    private void readData(){
         EmployeePayRoll employeePayRoll=new EmployeePayRoll();
         System.out.println("Enter your Id");
-        int id=scan.nextInt();
-        employeePayRoll.setId(id);
+        employeePayRoll.setId(scan.nextInt());
         System.out.println("Enter your name");
-        String name=scan.next();
-        employeePayRoll.setName(name);
+        employeePayRoll.setName(scan.next());
         System.out.println("Enter your salary");
-        double salary=scan.nextDouble();
-        employeePayRoll.setSalary(salary);
-        list.add(employeePayRoll);
-        print();
+        employeePayRoll.setSalary(scan.nextDouble());
+        employeePayRolls.add(employeePayRoll);
     }
-    public static void print() {
-        Iterator<EmployeePayRoll> it = list.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
+    private void writeData(){
+        System.out.println(employeePayRolls);
     }
+
 }
